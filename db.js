d1b52a5f9ce5de665bcc153492c8164b2df17862
Letícia Ribeiro <103691985+leticiaribeiro16@users.noreply.github.com>
2023-11-26 20:21:39 -0300

    Configurando banco azure e adicionando certificado ssl

--- db.js
+++ db.js
@@ -1,15 +1,32 @@
 const mysql = require('mysql');
+const fs = require('fs');
+const express = require('express')
 
-const connection = mysql.createConnection({
-  host: 'http://bdminerva.mysql.database.azure.com',
-  user: 'bdminerva',
-  password: 'AdoAdoAdo123!#$',
-  database: 'minerva'
+const app = express();
+
+const serverCa = [fs.readFileSync("./ssl/DigiCertGlobalRootCA.crt.pem", "utf8")];
+const db = mysql.createConnection({
+    host: "bdminerva.mysql.database.azure.com",
+    user: "minerva",
+    password: "AdoAdoAdo123!#$",
+    database: "minerva",
+    port: 3306,
+    ssl: {
+        rejectUnauthorized: true,
+        ca: serverCa
+    }
 });
 
-connection.connect((err) => {
-  if (err) throw err;
-  console.log('Connected to the database!');
+app.get('/testeConection', (req, res) => {
+    db.connect((err) => {
+        if (err) {
+            console.error('Erro ao conectar ao MySQL:', err);
+            res.status(500).send('Erro ao conectar MySQL');
+        } else {
+            console.log('Conexão bem sucedida ao MySQL');
+            res.send('Conexão bem sucedida ao MySQL');
+        }
+    });
 });
 
-module.exports = connection;
+module.exports = db;
