14c624393ed5f8587c478df29d7817a538fbc988
LopesZ3R4 <jl.moreno7@hotmail.com>
2023-12-05 19:31:32 -0400

    new tables migration

--- db.js
+++ db.js
@@ -59,5 +59,77 @@ db.query('SELECT 1 FROM users LIMIT 1', (err, results) => {
         });
     }
 });
+db.query('SELECT 1 FROM disciplina LIMIT 1', (err, results) => {
+    if(err) {
+        console.log('Disciplina table does not exist, initializing...');
+        db.query(`CREATE TABLE disciplina (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            nome VARCHAR(255) NOT NULL,
+            descricao VARCHAR(255),
+            carga_horaria INT
+        );`, (err, results) => {
+            if(err) {
+                console.error('Error initializing users table:', err);
+            } else {
+                console.log('Disciplina table initialized successfully');
+            }
+        });
+    }
+});
 
+db.query('SELECT 1 FROM demanda LIMIT 1', (err, results) => {
+    if(err) {
+        console.log('demanda table does not exist, initializing...');
+        db.query(`CREATE TABLE demanda (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            id_disciplina INT,
+            orientador VARCHAR(255),
+            qnt_bolsas INT,
+            requisitos VARCHAR(255),
+            FOREIGN KEY (id_disciplina) REFERENCES disciplina(id),
+            FOREIGN KEY (orientador) REFERENCES users(matricula)
+        );`, (err, results) => {
+            if(err) {
+                console.error('Error initializing users table:', err);
+            } else {
+                console.log('demanda table initialized successfully');
+            }
+        });
+    }
+});
+db.query('SELECT 1 FROM inscricao LIMIT 1', (err, results) => {
+    if(err) {
+        console.log('inscricao table does not exist, initializing...');
+        db.query(`CREATE TABLE inscricao (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            id_user VARCHAR(255),
+            aprovado BOOLEAN,
+            nota FLOAT,
+            FOREIGN KEY (id_user) REFERENCES users(matricula)
+        );`, (err, results) => {
+            if(err) {
+                console.error('Error initializing users table:', err);
+            } else {
+                console.log('inscricao table initialized successfully');
+            }
+        });
+    }
+});
+db.query('SELECT 1 FROM edital LIMIT 1', (err, results) => {
+    if(err) {
+        console.log('edital table does not exist, initializing...');
+        db.query(`CREATE TABLE edital (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            titulo VARCHAR(255),
+            id_demanda INT,
+            FOREIGN KEY (id_demanda) REFERENCES demanda(id)
+        );`, (err, results) => {
+            if(err) {
+                console.error('Error initializing users table:', err);
+            } else {
+                console.log('edital table initialized successfully');
+            }
+        });
+    }
+});
 module.exports = db;
